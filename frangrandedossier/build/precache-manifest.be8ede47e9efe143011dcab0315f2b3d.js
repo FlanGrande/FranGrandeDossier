b2e927eff38038bbe1ed00878ca2f0c3f94995dc
FlanGrande <francisjgrande@gmail.com>
2020-07-27 20:29:32 +0100

    Added build folder.

---
+++ frangrandedossier/build/precache-manifest.be8ede47e9efe143011dcab0315f2b3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f95f7054756024d13ff22159c127ce28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a61272447e631086fc3d",
+    "url": "/static/css/main.df5c753b.chunk.css"
+  },
+  {
+    "revision": "81e83c6715a29767e00d",
+    "url": "/static/js/2.61f5f1f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.61f5f1f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a61272447e631086fc3d",
+    "url": "/static/js/main.e2bc8017.chunk.js"
+  },
+  {
+    "revision": "ece8d2255fe32ce4515a",
+    "url": "/static/js/runtime-main.1e89bdde.js"
+  },
+  {
+    "revision": "af1a39b5c0e4b5dd09448713d0949836",
+    "url": "/static/media/door_reflection.af1a39b5.png"
+  }
+]);
