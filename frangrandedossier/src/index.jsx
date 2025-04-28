628576beb69a5136e8fdc752aefd6a1be33f446c
FlanGrande <francisjgrande@gmail.com>
2025-04-28 20:33:53 +0100

    Refactor using bun now

--- frangrandedossier/src/index.jsx
+++ frangrandedossier/src/index.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Route, Link, Switch, BrowserRouter as Router } from 'react-router-dom'
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import CV from './CV/CV';
-import * as serviceWorker from './serviceWorker';
 
-const routes = (
-	<Router>
-		<Switch>
-			<Route exact path="/" component={App} />
-			<Route path="/cv" component={CV} />
-			<Route exact path="/ThePerlness" render={() => {window.location.href="/ThePerlness/game.html"}} />
-		</Switch>
-	</Router>
+function PerlnessRedirect() {
+  useEffect(() => {
+    window.location.href = '/ThePerlness/game.html';
+  }, []);
+  return null;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/cv" element={<CV />} />
+      <Route path="/ThePerlness" element={<PerlnessRedirect />} />
+    </Routes>
+  </Router>
 );
-
-ReactDOM.render(routes, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
